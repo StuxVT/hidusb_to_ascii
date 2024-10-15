2960c7d5e17ea0e2f76c7d336492ad66918c7896
Stux <100985218+StuxVT@users.noreply.github.com>
2024-10-15 15:09:53 -0400

    python 3

--- hidusb_to_ascii.py
+++ hidusb_to_ascii.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 """
 Try and translate a pcap of USB HID to ascii text.
@@ -7,25 +7,19 @@ The data is in "USB Leftover section" - either use wireshark or tshark directly 
 tshark -r ./my.pcapng -T fields -e usb.capdata -Y usb.capdata 2>/dev/null
 
 The output should look like:
-00:00:25:00:00:00:00:00
-00:00:00:00:00:00:00:00
-00:00:21:00:00:00:00:00
-00:00:00:00:00:00:00:00
-00:00:26:00:00:00:00:00
-00:00:00:00:00:00:00:00
-00:00:22:00:00:00:00:00
-00:00:00:00:00:00:00:00
+0000000000000000
+0000040000000000
+0000000000000000
+00000b0000000000
 
-The first is used for modifiers, we unly use 0x20 here. (shift)
-The third is the keycode for the key.
-The forth is errorbit - here used for long keypresses
+The first byte is used for modifiers, we only use 0x02 here. (shift)
+The third byte is the keycode for the key.
+The fourth byte is errorbit - here used for long keypresses
 
 If all is 00, this is a key release
 
 """
 import string
-import sys
-
 
 def usb_to_ascii(num, mod=0):
     # map keys
@@ -35,26 +29,32 @@ def usb_to_ascii(num, mod=0):
     # Default to lower
     chars = lower
     
-    if mod == 32:
+    if mod == 2:  # Changed from 32 to 2
         chars = upper
     if num == 42:
         # Hack in backspace
         return '\x08'
-    if 0<= num < len(chars):
-        # print num, chars[num]
+    if 0 <= num < len(chars):
+        # print(num, chars[num])
         return chars[num]
 
 text = ""
-for line in sys.stdin.readlines():
-    mod, spam, val, err = line.split(":")[:4] # Only use for 4 first
-    # Convert from base 16
-    val = int(val, 16)
-    mod = int(mod, 16)
-    err = int(err, 16)
-    if err is 0: # Only append if error bit isn't set (This will exclude long keypresses)
-        if val: # Only continue if this is not a keyrelease
-            char = usb_to_ascii(val, mod=mod)
-            if char is not None:
-                text += char
+with open('./keyboard.txt', 'r') as file:
+    for line in file:
+        # Parse the line as a single 16-character hexadecimal string
+        data = line.strip()
+        if len(data) != 16:
+            continue  # Skip invalid lines
+        
+        # Extract relevant bytes
+        mod = int(data[0:2], 16)
+        val = int(data[4:6], 16)
+        err = int(data[6:8], 16)
+        
+        if err == 0:  # Only append if error bit isn't set (This will exclude long keypresses)
+            if val:  # Only continue if this is not a keyrelease
+                char = usb_to_ascii(val, mod=mod)
+                if char is not None:
+                    text += char
 
-print text
+print(text)
